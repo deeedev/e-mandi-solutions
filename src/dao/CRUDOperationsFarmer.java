d5e6a3a1e659334b26cb5250558af3cfee5afefa
Deepankar <61615789+lemecode@users.noreply.github.com>
2020-10-05 00:53:46 +0530

    Completed

--- src/dao/CRUDOperationsFarmer.java
+++ src/dao/CRUDOperationsFarmer.java
@@ -1,14 +1,22 @@
 package dao;
 
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
+import model.Crops;
 import model.Farmer;
+import utility.FeedBack;
+import utility.License;
+import utility.Trade;
 
-public class CRUDOperationsFarmer {
+public class CRUDOperationsFarmer extends FeedBack implements License, Trade {
+
+	Crops crop = new Crops();
 
 	static Map<String, Farmer> mapFarmer = new TreeMap<String, Farmer>();
+	Scanner sc = new Scanner(System.in);
 
 	public void add(Farmer fam1) {
 		// TODO Auto-generated method stub
@@ -52,4 +60,84 @@ public class CRUDOperationsFarmer {
 		} else
 			System.out.println("License Number is Not Registered!!\n");
 	}
+
+	@Override
+	public void licenseRenewal() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter your Name: ");
+		String name = sc.nextLine();
+
+		System.out.println("Enter your phone number: ");
+		String phone = sc.nextLine();
+
+		System.out.println("Enter your License Number: ");
+		String ln = sc.nextLine();
+
+		if (mapFarmer.containsKey(ln)) {
+
+			Farmer fam = new Farmer();
+			fam.setName(name);
+			fam.setPhoneNumber(phone);
+			fam.setLicenseNumber(ln);
+
+			licenseRenewalFarmer.put(ln, fam);
+		} else
+			System.out.println("The License Number is not Valid");
+
+	}
+
+	@Override
+	public void Buy() {
+		// TODO Auto-generated method stub
+
+		if (Trade.buyOrder.size() != 0)
+			for (String key : Trade.buyOrder.keySet())
+				System.out.println("Crop Name: " + key + "\nPrice: " + Trade.buyOrder.get(key).getPrice()
+						+ "\nQuantity: " + Trade.buyOrder.get(key).getQuantity());
+		else
+			System.out.println("*** No Buy Order from WholeSeller ***");
+
+	}
+
+	@Override
+	public void Sell() {
+		// TODO Auto-generated method stub
+
+		int j = 1;
+
+		String arr[] = crop.getCropsList();
+
+		for (int i = 0; i < crop.getCropsList().length; i++) {
+			System.out.println(j + ". " + arr[i]);
+			j++;
+		}
+
+		System.out.println("\nEnter your choice: ");
+
+		int c1 = sc.nextInt();
+
+		System.out.println("Crops MSP: " + CRUDOperationCrops.getMapCropsMSP().get(arr[c1 - 1]).getMsp()
+				+ "\nCrop Name: " + arr[c1 - 1]);
+		System.out.println();
+
+		System.out.println("Enter Buy Price");
+		double bp = sc.nextDouble();
+		System.out.println("Enter Quantity: ");
+		double quantity = sc.nextDouble();
+
+		System.out.println("***** Buy Order Sent To Dashboard Successfully *****");
+
+		System.out.println("\nCrop Name: " + arr[c1 - 1] + "\nBuy Price: " + bp + "\nQuantity: " + quantity + "\n");
+
+		Crops crp = new Crops();
+
+		crp.setName(arr[c1 - 1]);
+		crp.setPrice(bp);
+		crp.setQuantity(quantity);
+
+		Trade.sellOrderFarmer.put(crp.getName(), crp);
+
+	}
+
 }
--- src/model/Farmer.java
+++ src/model/Farmer.java
@@ -3,8 +3,14 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class Farmer extends Crops {
+import dao.CRUDOperationCrops;
+import dao.CRUDOperationsFarmer;
+import utility.FeedBack;
+import utility.License;
+
+public class Farmer extends FeedBack {
 
 	String name;
 	String phoneNumber;
@@ -12,8 +18,9 @@ public class Farmer extends Crops {
 	String licenseNumber;
 
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	Scanner sc = new Scanner(System.in);
 	Crops crop = new Crops();
+	CRUDOperationsFarmer fam = new CRUDOperationsFarmer();
 
 	public Farmer() {
 
@@ -50,50 +57,138 @@ public class Farmer extends Crops {
 		this.address = address;
 	}
 
+	@Override
 	public String getLicenseNumber() {
 		return licenseNumber;
 	}
 
+	@Override
 	public void setLicenseNumber(String licenseNumber) {
 		this.licenseNumber = licenseNumber;
 	}
 
 	public void farmerLogin() throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
+		int ch;
 
-		System.out.println("<--------------------------------------------------->");
-		System.out.println("<         Welcome to the Farmer Portal               ");
-		System.out.println("<--------------------------------------------------->");
+		do {
 
-		System.out.println("1. Sell Corps");
-		System.out.println("2. Know Minimum Support Price");
-		System.out.println("3. FeedBack/Complaint");
-		System.out.println("4. Know your previous transcations");
+			System.out.println("<--------------------------------------------------->");
+			System.out.println("<         Welcome to the Farmer Portal               ");
+			System.out.println("<--------------------------------------------------->");
 
-		int choice = Integer.parseInt(br.readLine());
+			System.out.println("1. Sell Corps");
+			System.out.println("2. Know Minimum Support Price");
+			System.out.println("3. Apply for License Renewal/Status");
+			System.out.println("4. FeedBack/Complaint");
+			System.out.println("5. Know your previous transcations");
+			System.out.println("6. Exit");
 
-		switch (choice) {
-		case 1:
+			int choice = Integer.parseInt(br.readLine());
+			ch = choice;
 
-			System.out.println("Select Crops from the below list\n");
-
-			System.out.println("\nEnter your choice: ");
-			int c = Integer.parseInt(br.readLine());
-
-			switch (c) {
+			switch (choice) {
 			case 1:
-//				System.out.println("Current "+ );
+
+				System.out.println("1. Buy Order Posted by WholeSeller");
+				System.out.println("2. Post your sell order");
+				System.out.println("3. Exit");
+				System.out.println("Enter your choice: ");
+
+				int ch1 = Integer.parseInt(br.readLine());
+
+				switch (ch1) {
+				case 1:
+
+					fam.Buy();
+
+					break;
+
+				case 2:
+
+					fam.Sell();
+
+				case 3:
+					break;
+
+				default:
+					System.out.println("Invalid Input");
+					break;
+				}
+
 				break;
 
-			default:
+			case 2:
+
+				int j = 1;
+
+				String arr[] = crop.getCropsList();
+
+				for (int i = 0; i < crop.getCropsList().length; i++) {
+					System.out.println(j + ". " + arr[i]);
+					j++;
+				}
+
+				System.out.println("\nPlease Enter your choice to know the Minimum Support Price: ");
+
+				int c1 = sc.nextInt();
+
+				System.out.println("\nCrop Name: " + arr[c1 - 1] + "Crops MSP: "
+						+ CRUDOperationCrops.getMapCropsMSP().get(arr[c1 - 1]).getMsp());
 				break;
+
+			case 3:
+
+				System.out.println("1. Apply for License Renew");
+				System.out.println("2. Check Status");
+				System.out.println("Enter your choice: ");
+
+				int ch2 = Integer.parseInt(br.readLine());
+
+				switch (ch2) {
+				case 1:
+					fam.licenseRenewal();
+					System.out.println("*** Request has been sent ***\n");
+					break;
+
+				case 2:
+					System.out.println("Enter your License Number: ");
+					String ln = br.readLine();
+
+					if (CRUDOperationsFarmer.getMapFarmer().containsKey(ln))
+						System.out.println("License Renewal Status: " + License.licenseRenewalstatus.get(ln));
+					else
+						System.out.println("License number is not registered!!");
+				default:
+					break;
+				}
+				break;
+
+			case 4:
+
+				System.out.println("1.Feedback");
+				System.out.println("2.Complaint");
+				System.out.println("Enter your choice: ");
+
+				int ch3 = sc.nextInt();
+
+				switch (ch3) {
+				case 1:
+					fillFeedback();
+					System.out.println("**** Thanks for your valuable feedback ****\n");
+					break;
+
+				case 2:
+					fillComplaint();
+					System.out.println("**** I am sorry for any inconvenience caused to you ****\n");
+					break;
+
+				default:
+					// System.out.println("Invalid Input");
+					break;
+				}
+
 			}
-			break;
-
-		default:
-			break;
-		}
-
+		} while (ch != 6);
 	}
-
 }
